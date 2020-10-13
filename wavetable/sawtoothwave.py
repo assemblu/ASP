a7f4b9e2a51298cf7a8c61cf51fa1757b6a5992c
Emir <40762681+emirgo@users.noreply.github.com>
2020-10-13 23:47:34 +0200

    sawtooth

---
+++ wavetable/sawtoothwave.py
@@ -0,0 +1,17 @@
+# Author: Emirhan Gocturk
+# Description: Triangular wave lookup table data dumper
+
+from scipy import signal
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0, 6.5, 0.1)
+y = signal.sawtooth(t)
+plt.plot(t, y)  
+plt.grid()
+plt.xlabel("Time [s]")
+plt.ylabel("Amplitude [V]")
+plt.title("Sawtooth Wave Sample")
+plt.show()
+
+print("DATA DUMP= ", y)
