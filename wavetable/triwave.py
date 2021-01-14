0b49a6ebf138719223e5c36e7e974102c443b7da
Emir <40762681+emirgo@users.noreply.github.com>
2021-01-14 12:32:56 +0100

    Range and conversion to int

--- wavetable/triwave.py
+++ wavetable/triwave.py
@@ -6,12 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 t = np.arange(0, np.pi*2, 0.00156)
-y = signal.sawtooth(t, 0.5)
+y = 4000/2+signal.sawtooth(t, 0.5)*4000/2
+
+index = 0
+while index < len(y):
+    y[index] = int(y[index])
+    index += 1
+
 plt.plot(t, y)  
 plt.grid()
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude [V]")
 plt.title("Tri Wave Sample")
 plt.show()
-
-print("DATA DUMP= ", y)
