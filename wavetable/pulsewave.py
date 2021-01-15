cc54582aeb30f0b9261704b7061d17647b53fc8f
Emir <40762681+emirgo@users.noreply.github.com>
2021-01-15 19:50:10 +0100

    Convert to int

--- wavetable/pulsewave.py
+++ wavetable/pulsewave.py
@@ -7,6 +7,12 @@ import numpy as np
 
 t = np.arange(0, np.pi*2, 0.001534)
 y = signal.square(t, 0.5)
+
+index = 0
+while index < len(y):
+    y[index] = int(y[index])
+    index += 1
+
 plt.plot(t, y)  
 plt.grid()
 plt.xlabel("Time [s]")
