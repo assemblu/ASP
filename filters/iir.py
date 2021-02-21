8752845d803a299d144a644ee30b18415bdf4b42
Emir <40762681+emirgo@users.noreply.github.com>
2021-02-20 16:54:20 -0800

    Commentary

--- filters/iir.py
+++ filters/iir.py
@@ -2,11 +2,19 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
-b, a = signal.butter(9, 0.125)
+# Butter worth filter creation
+b, a = signal.butter(8, 0.125)
 
+# Sample count
 n = 60
+
+# Noise signla
 sig = np.random.randn(n)**3 + 3*np.random.randn(n).cumsum()
+
+# Back and forth filtering
 fpad = signal.filtfilt(b, a, sig, padlen=50)
+
+# Visualize
 plt.plot(sig, 'k-', label='input')
 plt.plot(fpad, 'c-', linewidth=1.5, label='pad')
 plt.legend(loc='best')
