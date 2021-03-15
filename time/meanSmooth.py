5a5914dd3fe0c9acb6d43d9351115de76276dad8
Emir <40762681+emirgo@users.noreply.github.com>
2021-03-15 23:24:02 +0100

    Signal and noise generation

---
+++ time/meanSmooth.py
@@ -0,0 +1,23 @@
+# Author: Emirhan Gocturk
+# Description: Running mean smooth filter algorithm 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as signal
+
+# Pure signal
+x = np.arange(0, 10, 0.1)
+y = np.sin(x)
+
+# Noise at time length
+noise = np.random.normal(0, 1, len(x))
+
+y = y + noise
+
+
+# Window
+plt.plot(x, y)
+plt.grid()
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.show()
