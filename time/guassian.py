e5603912f99b53ed3b51b4ed63dca8797188e69a
Emir <40762681+emirgo@users.noreply.github.com>
2021-03-27 00:13:54 +0100

    Guassian init

---
+++ time/guassian.py
@@ -0,0 +1,11 @@
+# Author: Emirhan Gocturk
+# Date: 27 March 2021
+# Description: Guassian mean algorithm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import scipy.signal
+from scipy import *
+import copy
+
