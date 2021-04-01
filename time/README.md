d3f34e112e28ff1fd210f4474e7fd2692fa41605
Emir <40762681+emirgo@users.noreply.github.com>
2021-04-02 01:17:28 +0200

    Gaussian progress somewhat

--- time/README.md
+++ time/README.md
@@ -1,2 +1,5 @@
 # Time Series
 This directory includes digital signal algorithms which revolve around time series.
+
+# Mean
+Default kernel is 20 meaning that it goes back and forth 20 samples, takes an average and the current sample is set as that average value. Due to this calculation, edges are faulty.
--- time/gaussianMean.py
+++ time/gaussianMean.py
@@ -9,3 +9,30 @@ import scipy.signal
 from scipy import *
 import copy
 
+
+# signal creation
+srate = 1000 #Hz
+time = np.arange(0, 3, 1/srate)
+n = len(time)
+p = 15 #num of poles
+
+noiseamp = 5
+
+ampl = np.interp(np.linspace(0, p, n), np.arange(0, p), np.random.rand(p)*30)
+noise = noiseamp * np.random.randn(n)
+signal = ampl + noise
+
+# init filtered signal
+filteredSignal = np.zeros(n)
+
+fwhm = 25 #ms
+k = 100
+#todo
+gtime = 0
+# gaussian
+gaussianWindow = numpy.exp(-(4*np.log(2)*gtime**2)/fwhm**2)
+
+
+plt.plot(time, signal, label="o")
+plt.plot(time, filteredSignal, label="f")
+plt.show()
