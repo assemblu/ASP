3c9c394b69db02fda62f11ce4b37c587aa383b19
Emir Gocturk <emir.gocturk@axign.nl>
2021-04-08 10:25:31 +0200

    Create fir.py

---
+++ filters/fir.py
@@ -0,0 +1,10 @@
+# Author: Emirhan Gocturk
+# Description: FIR Filter
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import scipy.signal
+from scipy import *
+import copy
+
