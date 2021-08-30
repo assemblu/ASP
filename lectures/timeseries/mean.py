965da788eed510de4fdd0fd73ae9a28f717bc9b5
Emir <40762681+emirgo@users.noreply.github.com>
2021-08-30 23:37:44 +0200

    Initial boilerplate

--- lectures/timeseries/mean.py
+++ lectures/timeseries/mean.py
@@ -1,4 +1,16 @@
 # Author: Emirhan Gocturk
 # Description: Mean smooth algorithm
 
-# ...
+import numpy as np
+
+sampling_rate = 1000 #Hz
+# 0 to 3 chop into 1/1000
+time = np.arange(0, 3, 1/sampling_rate)
+# sample count
+n = len(time) 
+# number of poles, for interpolation
+p = 15
+
+# artifically added noise
+noise_amplitude = 5
+
