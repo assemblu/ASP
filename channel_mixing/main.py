453f03b206977ed2fdd9bb923834c4f802a34400
Emir Gocturk <40762681+emirgo@users.noreply.github.com>
2022-02-01 15:01:02 +0100

    Decide which channel is longer

--- channel_mixing/main.py
+++ channel_mixing/main.py
@@ -24,9 +24,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
 
-
+# read channels both at 44100 Hz
 fs_channel_1, data_channel_1 = wavfile.read('C:\dev\ASP\channel_mixing\samples\synth.wav')
 fs_channel_2, data_channel_2 = wavfile.read('C:\dev\ASP\channel_mixing\samples\piano.wav')
 
+# measure length
+length_channel_1 = len(data_channel_1)
+length_channel_2 = len(data_channel_2)
+
+sample_length = 0
+# match sample length
+if length_channel_1 > length_channel_2:
+    # ch1 is longer than ch2
+    sample_length = length_channel_2
+else:
+    sample_length = length_channel_1
 
 
+# mix two channels
+
+
+# plot result
+plt.plot(data_channel_2)
+plt.show()
